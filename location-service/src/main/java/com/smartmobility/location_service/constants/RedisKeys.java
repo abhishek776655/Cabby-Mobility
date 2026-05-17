f8cd16730dae378df121655cd41fe7dc9bd1f6c3
abhishek776655 <abhishek.pandey9868@gmail.com>
2026-05-17 12:48:08 +0530

    feat: add DRIVERS_AVAILABLE_GEO key constant

--- location-service/src/main/java/com/smartmobility/location_service/constants/RedisKeys.java
+++ location-service/src/main/java/com/smartmobility/location_service/constants/RedisKeys.java
@@ -4,6 +4,7 @@ public class RedisKeys {
 
     public static final String DRIVERS_GEO = "drivers:geo";
     public static final String DRIVERS_AVAILABLE = "drivers:available";
+    public static final String DRIVERS_AVAILABLE_GEO = "drivers:available:geo";
 
     private RedisKeys() {}
 }
